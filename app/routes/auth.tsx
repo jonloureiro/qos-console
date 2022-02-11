6016465fa4f2d76912b4bb530c73e2d160e1a23b
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2022-02-11 11:47:25 -0400

    chore: init login form

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,12 +1,24 @@
+import { Form } from "remix";
+
 export default function AuthPage() {
   return (
-    <div className="">
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Username</span>
+    <div className=" flex justify-center items-center h-full w-full">
+
+      <Form className="h-auto max-w-xs">
+        <h1 className="text-4xl font-bold text-zinc-700">Login</h1>
+        <label className="label flex-wrap">
+          <span className="label-text">Email</span>
+          <input type="email" className="input input-primary w-full" />
         </label>
-        <input type="text" placeholder="username" className="input" />
-      </div>
+
+        <label className="label flex-wrap">
+          <span className="label-text">Senha</span>
+          <input type="password" className="input input-primary w-full" />
+        </label>
+
+        <button type="submit" className="btn btn-primary btn-block">Entrar</button>
+      </Form>
+
     </div>
   )
 }
