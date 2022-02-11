b029f90b13b225944289e5fffa07c74bbd49901d
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2022-02-11 10:31:53 -0400

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,40 @@
+import {
+  Links,
+  LinksFunction,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "remix";
+import type { MetaFunction } from "remix";
+
+import styles from "./style.css";
+
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles }
+]
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+
+export default function App() {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./app/**/*.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
