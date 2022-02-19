52d9e39ea8eb9cc02e391b8acb87c2b206c01f07
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2022-02-18 23:05:18 -0400

    fix path

--- server/main.ts
+++ server/main.ts
@@ -1,14 +1,14 @@
 import { Handler } from "@netlify/functions";
 import * as Eta from "eta"
-import path from "path";
+import { resolve } from "path";
 
 const handler: Handler = async (event, context) => {
   Eta.configure({
-    views: path.join(__dirname, '..', '..', '..', '..', '..', 'views')
+    views: resolve('views')
   })
   return {
     statusCode: 200,
-    body: await Eta.renderFile('home', { message: 'Hello World' }) as string,
+    body: await Eta.renderFile('../views/home.eta', { message: 'Hello World' }) as string,
   };
 };
 
