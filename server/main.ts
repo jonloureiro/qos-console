3d30be3f7f941c55de33e2dbaba896979be03b46
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2022-02-21 00:11:00 -0400

    lint and refactor

--- server/main.ts
+++ server/main.ts
@@ -1,39 +1,31 @@
 import { resolve } from 'path'
 import { createSign, createVerify } from 'crypto'
 
-import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions'
+import { HandlerEvent, HandlerContext } from '@netlify/functions'
 import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb'
 import * as Eta from 'eta'
 
 import users from '../data/users'
 
-
-type Item = {
+interface Item {
   hora: {
     S: string
-  },
+  }
   device_id: {
     S: string
-  },
+  }
   registry: {
     N: string
-  },
+  }
   qos: {
     S: string
-  },
+  }
   data: {
     S: string
   }
 }
 
-
-type Result = [number, number, number]
-
-
-Eta.configure({
-  views: resolve('views')
-})
-
+Eta.configure({ views: resolve('views') })
 
 const client = new DynamoDBClient({
   region: process.env.DYNAMODB_REGION!,
@@ -43,40 +35,42 @@ const client = new DynamoDBClient({
   }
 })
 
-
-const privateKey = Buffer.from(process.env.PRIVATE_KEY, 'base64').toString()
-const publicKey = Buffer.from(process.env.PUBLIC_KEY, 'base64').toString()
-
+const privateKey = Buffer.from(process.env.PRIVATE_KEY!, 'base64').toString()
+const publicKey = Buffer.from(process.env.PUBLIC_KEY!, 'base64').toString()
 
 const main = async (event: HandlerEvent, context: HandlerContext) => {
-  let userEmail: string | undefined = undefined
+  let userEmail: string | undefined
   if (event.headers.cookie) {
     const cookies = event.headers.cookie.split(';')
 
     const sessionCookie = cookies.find(value => {
-      const [cookieKey,] = value.trim().split('=')
+      const [cookieKey] = value.trim().split('=')
       return cookieKey === '__session'
     })
 
     if (sessionCookie) {
       const [, cookieValue] = sessionCookie.split('=')
 
-      if (!cookieValue) throw {
-        statusCode: 400,
-        errorMessage: 'Cookie inválido'
+      if (!cookieValue) {
+        throw {
+          statusCode: 400,
+          errorMessage: 'Cookie inválido'
+        }
       }
 
       const [emailHex, token] = cookieValue.split('&')
 
-      if (!emailHex || !token) throw {
-        statusCode: 400,
-        errorMessage: 'Cookie inválido'
+      if (!emailHex || !token) {
+        throw {
+          statusCode: 400,
+          errorMessage: 'Cookie inválido'
+        }
       }
 
       const email = Buffer.from(emailHex, 'hex').toString()
 
-      const verifier = createVerify('rsa-sha256');
-      verifier.update(email);
+      const verifier = createVerify('rsa-sha256')
+      verifier.update(email)
       const hasUser = verifier.verify(publicKey, token, 'hex')
       if (hasUser) {
         userEmail = email
@@ -85,22 +79,26 @@ const main = async (event: HandlerEvent, context: HandlerContext) => {
   }
 
   if (event.path === '/login' && event.httpMethod === 'GET') {
-    if (userEmail) return {
-      statusCode: 302,
-      headers: {
-        'Location': '/'
+    if (userEmail) {
+      return {
+        statusCode: 302,
+        headers: {
+          Location: '/'
+        }
       }
     }
     return {
       statusCode: 200,
-      body: await Eta.renderFile('login.eta', {}) as string,
+      body: await Eta.renderFile('login.eta', {}) as string
     }
   }
 
   if (event.path === '/login' && event.httpMethod === 'POST') {
-    if (!event.body) throw {
-      statusCode: 400,
-      errorMessage: 'Tente novamente'
+    if (!event.body) {
+      throw {
+        statusCode: 400,
+        errorMessage: 'Tente novamente'
+      }
     }
 
     const [emailstring, passwordstring] = decodeURIComponent(event.body).split('&')
@@ -109,67 +107,73 @@ const main = async (event: HandlerEvent, context: HandlerContext) => {
 
     const user = users.find(user => (user && user.email && user.email === email))
 
-    if (user && user.password === password) {
+    if ((user != null) && user.password === password) {
       const signer = createSign('rsa-sha256')
       signer.update(user.email)
       const token = signer.sign(privateKey, 'hex')
-      const email = Buffer.from(user.email).toString('hex')
+      const emailHex = Buffer.from(user.email).toString('hex')
 
       return {
         statusCode: 200,
         headers: {
-          'Set-Cookie': `__session=${email + '&' + token}; Max-Age=3600; ${process.env.NODE_ENV !== 'development' ? 'Secure;' : ''} HttpOnly; SameSite=Lax;`
+          'Set-Cookie': `__session=${emailHex + '&' + token}; Max-Age=3600; ${process.env.NODE_ENV !== 'development' ? 'Secure; ' : ''}HttpOnly; SameSite=Lax;`
         },
-        body: await Eta.renderFile('_redirect.eta', { redirect: '/', message: 'Login com sucesso' }) as string,
+        body: await Eta.renderFile('_redirect.eta', { redirect: '/', message: 'Login com sucesso' }) as string
       }
     }
 
     return {
       statusCode: 401,
-      body: await Eta.renderFile('login.eta', { errorMessage: 'Email ou senha incorretos' }) as string,
+      body: await Eta.renderFile('login.eta', { errorMessage: 'Email ou senha incorretos' }) as string
     }
   }
 
-  if (event.path === '/logout' && event.httpMethod === 'POST') return {
-    statusCode: 200,
-    headers: {
-      'Set-Cookie': `__session=; Max-Age=0; ${process.env.NODE_ENV !== 'development' ? 'Secure;' : ''} HttpOnly; SameSite=Lax;`
-    },
-    body: await Eta.renderFile('_redirect.eta', { redirect: '/login', message: 'Logout com sucesso' }) as string,
+  if (event.path === '/logout' && event.httpMethod === 'POST') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Set-Cookie': `__session=; Max-Age=0; ${process.env.NODE_ENV !== 'development' ? 'Secure; ' : ''}HttpOnly; SameSite=Lax;`
+      },
+      body: await Eta.renderFile('_redirect.eta', { redirect: '/login', message: 'Logout com sucesso' }) as string
+    }
   }
 
-  if (event.path !== '/' && event.httpMethod === 'GET') throw {
-    statusCode: 404,
-    errorMessage: 'Página não encontrada'
+  if (event.path !== '/' && event.httpMethod === 'GET') {
+    throw {
+      statusCode: 404,
+      errorMessage: 'Página não encontrada'
+    }
   }
 
-  if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(event.httpMethod)) throw {
-    statusCode: 501,
-    errorMessage: 'Método não implementado'
+  if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(event.httpMethod)) {
+    throw {
+      statusCode: 501,
+      errorMessage: 'Método não implementado'
+    }
   }
 
   if (userEmail) {
     const command = new QueryCommand({
-      TableName: "totem_qos_table",
-      IndexName: "mes_ano-index",
-      KeyConditionExpression: "mes_ano = :month_year",
+      TableName: 'totem_qos_table',
+      IndexName: 'mes_ano-index',
+      KeyConditionExpression: 'mes_ano = :month_year',
       ExpressionAttributeValues: {
-        ":month_year": { S: "01-2022" } //TODO: existe itens com 02-2022 e 2-2022
-      },
+        ':month_year': { S: '01-2022' }
+      }
     })
 
-    const results = await client.send(command);
-    const items = (results.Items as Item[]) ?? []
+    const results = await client.send(command)
+    const items = (results.Items as unknown as Item[]) ?? []
 
     const qos = items.reduce((previousValue, currentValue) => {
       switch (currentValue.qos.S) {
-        case "RUIM":
+        case 'RUIM':
           previousValue[0]++
           return previousValue
-        case "REGULAR":
+        case 'REGULAR':
           previousValue[1]++
           return previousValue
-        case "EXCELENTE":
+        case 'EXCELENTE':
           previousValue[2]++
           return previousValue
         default:
@@ -177,35 +181,32 @@ const main = async (event: HandlerEvent, context: HandlerContext) => {
       }
     }, [0, 0, 0])
 
-    //TODO: Buscar os dados
     return {
       statusCode: 200,
-      body: await Eta.renderFile('home.eta', { email: userEmail, message: qos }) as string,
+      body: await Eta.renderFile('home.eta', { email: userEmail, message: qos }) as string
     }
   }
 
   return {
     statusCode: 302,
     headers: {
-      'Location': '/login'
+      Location: '/login'
     }
   }
 }
 
-
-const handler: Handler = async (event, context) => {
+const handler = async (event: HandlerEvent, context: HandlerContext) => {
   if (context) context.callbackWaitsForEmptyEventLoop = false
 
   try {
     return await main(event, context)
-  } catch (error) {
+  } catch (error: any) {
     console.error(error)
     return {
       statusCode: error.statusCode || 500,
-      body: await Eta.renderFile('external.eta', { message: error.errorMessage || 'Erro interno do servidor' }) as string,
+      body: await Eta.renderFile('external.eta', { message: error.errorMessage || 'Erro interno do servidor' }) as string
     }
   }
 }
-
 
 export { handler }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,54 +3,54 @@ module.exports = {
     './views/*.eta'
   ],
   corePlugins: {
-    fontFamily: false,
+    fontFamily: false
   },
   theme: {
     extend: {
       colors: {
-        'primary': '#96323A',
-        'light': '#FAF5F5',
-        'cinza1': '#7a7a7a',
-        'cinza2': '#54595f',
-        'verde1': '#61CE70',
-        'verde2': '#23A455',
-        'azul': '#4054b2',
-        'vermelho': '#EE1C25',
+        primary: '#96323A',
+        light: '#FAF5F5',
+        cinza1: '#7a7a7a',
+        cinza2: '#54595f',
+        verde1: '#61CE70',
+        verde2: '#23A455',
+        azul: '#4054b2',
+        vermelho: '#EE1C25',
         'vermelho-claro': '#F9EAEB',
-        'laranja': '#F4A261'
+        laranja: '#F4A261'
       }
-    },
+    }
   },
   plugins: [
-    require('daisyui'),
+    require('daisyui')
   ],
   daisyui: {
     logs: false,
     themes: [
       {
         light: {
-          'primary': '#96323a',
+          primary: '#96323a',
           'primary-focus': '#96323a',
           'primary-content': '#FAF5F5',
-          'secondary': '#54595f',
+          secondary: '#54595f',
           'secondary-focus': '#54595f',
           'secondary-content': '#FAF5F5',
-          'accent': '#61ce70',
+          accent: '#61ce70',
           'accent-focus': '#61ce70',
           'accent-content': '#FAF5F5',
-          'neutral': '#7a7a7a',
+          neutral: '#7a7a7a',
           'neutral-focus': '#7a7a7a',
           'neutral-content': '#FAF5F5',
           'base-100': '#ffffff',
           'base-200': '#FAF5F5',
           'base-300': '#d1d5db',
           'base-content': '#1f2937',
-          'info': '#4054b2',
-          'success': '#23a455',
-          'warning': '#f4a261',
-          'error': '#ee1c25',
-        },
-      },
-    ],
-  },
+          info: '#4054b2',
+          success: '#23a455',
+          warning: '#f4a261',
+          error: '#ee1c25'
+        }
+      }
+    ]
+  }
 }
