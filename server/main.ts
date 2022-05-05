bb64c5f1067ce3b6b230ccca2b168e5c32f10803
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2022-05-04 23:18:56 -0400

    chore: add download button

--- server/main.ts
+++ server/main.ts
@@ -236,9 +236,8 @@ const main = async (event: HandlerEvent, context: HandlerContext) => {
     }, [0, 0, 0])
 
     const sheet = [
-      ["A1", "B1", "C1"],
-      ["A2", "B2", "C2"],
-      ["A3", "B3", "C3"]
+      ["RUIM", "REGULAR", "EXCELENTE"],
+      qos
     ]
 
     return {
